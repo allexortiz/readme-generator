9ae1cb750768ae359a9b9dc2fae2ea8ee556e874
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-11-01 21:40:24 -0500

    created 2 questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,43 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is the title of your repository? (Required)',
+    name: 'title',
+
+    //make sure title is typed
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your repository title.');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'What is the description of your repository? (Required)',
+    name: 'description',
+    //make sure description is typed
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a description of the repository.');
+            return false;
+        }
+    }
+
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
