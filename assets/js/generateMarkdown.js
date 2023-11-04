3097c3375d8e740778b2b3e6d49448b0dbcbab57
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-11-04 16:57:51 -0500

    finished markdown and linked to index.js

--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -1,20 +1,43 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'None') {
+    return '';
+  } else {
+    return `[![License](https://img.shields.io/badge/${license}-blue.svg)]`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  } else {
+    return `(https://opensource.org/licenses/${license})`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return '';
+  } else {
+    return `${renderLicenseBadge(license)}${renderLicenseLink(license)}`;
+  }
+}
+//[![License](https://img.shields.io/badge/Apache_2.0.svg)](https://opensource.org/licenses/Apache-2.0)
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `## ${data.title}
+${renderLicenseSection(data.license)}
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+
+//License-Apache_2.0-blue
+//Apache-2.0
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [{
     type: 'input',
@@ -31,13 +32,32 @@ const questions = [{
         }
     }
 
-}];
+},
+{
+    type: 'list',
+    message: 'What is your README badge? (Required)',
+    name: 'license',
+    choices: ['Apache-2.0', 'MIT', 'GPL 3.0', 'BSD 3', 'None']
+
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) { 
+    fs.writeFile(fileName, data, (err) => console.log(err))
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() { 
+    inquirer
+    .prompt(questions)
+    .then((data) =>{
+        writeToFile('My-ReadME.md', generateMarkdown(data))
+    }
+
+    )
+    .catch(err => console.log(err))
+}
 
 // Function call to initialize app
 init();
